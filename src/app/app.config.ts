1769ae5db449d9521ce478683216e679ef7f2637
wfuturi35 <wilfredofuturi1234567@gmail.com>
2025-07-06 09:07:02 -0500

    feat: creation view roles, and routes component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, withHashLocation} from '@angular/router';
 import { routes } from './app.routes';
 import {providePrimeNG} from 'primeng/config';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import Aura from '@primeng/themes/aura';
 import {MyPreset} from '../assets/styles/mypreset';
+import {MessageService} from "primeng/api";
+import {AuthGuard} from "./core/guards/auth.guard";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {tokenInterceptor} from "./core/interceptors/token.interceptor";
+import {errorInterceptor} from "./core/interceptors/error.interceptor";
+import {authInterceptor} from "./core/interceptors/auth.interceptor";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastModule} from 'primeng/toast';
+import {RoleGuard} from './core/guards/role.guard';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation()),
     provideAnimationsAsync(),
+    provideHttpClient(
+        withInterceptors([errorInterceptor, authInterceptor, tokenInterceptor]),
+    ),
+    AuthGuard,
+
+    MessageService,
+    importProvidersFrom([
+      BrowserAnimationsModule,
+      ToastModule
+    ]),
+
     providePrimeNG({
       theme: {
         preset: MyPreset,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,77 @@
 import { Routes } from '@angular/router';
+import {AuthGuard} from './core/guards/auth.guard';
+import {LoginComponent} from './features/auth/components/login/login.component';
+import {GuestGuard} from './core/guards/guest.guard';
+import {RoleGuard} from './core/guards/role.guard';
+import {UserRole} from './features/auth/models/user.model';
+import {LogoutComponent} from './shared/components/layout/logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'home', loadComponent: () => import('./shared/components/layout/home/home.component').then(m => m.HomeComponent) },
+    ]
+
+  },
+  { path: 'logout', component: LogoutComponent },
+  {
+    path: 'admin',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [UserRole.ADMIN] },
+    children: [
+      { path: 'users', loadComponent: () => import('./features/admin/users-management/users-management.component').then(m => m.UsersManagementComponent) },
+      { path: 'profile', loadComponent: () => import('./features/admin/profile/profile.component').then(m => m.ProfileComponent) }
+    ]
+  },
+
+  {
+    path: 'patient',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [UserRole.PATIENT] },
+    children: [
+      { path: 'my-appointments', loadComponent: () => import('./features/patient/my-appointment/my-appointment.component').then(m => m.MyAppointmentComponent) },
+      { path: 'book-appointments',
+        loadComponent: () => import('./features/patient/book-appointment/book-appointment.component').then(m => m.BookAppointmentComponent) ,
+        canActivate: [AuthGuard, RoleGuard],
+        data: { roles: [UserRole.PATIENT] },
+        children: [
+          { path: 'search-availability', loadComponent: () => import('./features/patient/book-appointment/search-availability/search-availability.component').then(m => m.SearchAvailabilityComponent) },
+        ]
+      },
+
+      { path: 'profile',
+        loadComponent: () => import('./features/patient/profile/profile.component').then(m => m.ProfileComponent),
+        /*canActivate: [AuthGuard, RoleGuard],
+        data: { roles: [UserRole.PATIENT] },
+        children: [
+          { path: 'book-appointments', loadComponent: () => import('./features/patient/book-appointment/book-appointment.component').then(m => m.BookAppointmentComponent) },
+        ]*/
+      },
+      { path: 'medical-history', loadComponent: () => import('./features/patient/medical-history/medical-history.component').then(m => m.MedicalHistoryComponent) },
+    ]
+  },
+  {
+    path: 'medic',
+    loadComponent: () =>  import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [UserRole.MEDIC] },
+    children: [
+      { path: 'profile', loadComponent: () => import('./features/medic/profile/profile.component').then(m => m.ProfileComponent) },
+      { path: 'schedule', loadComponent: () => import('./features/medic/my-calendar/my-calendar.component').then(m => m.MyCalendarComponent) },
+      { path: 'medical-history', loadComponent: () => import('./features/medic/clinical-history/clinical-history.component').then(m => m.ClinicalHistoryComponent) },
+    ]
+  },
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+
+];
